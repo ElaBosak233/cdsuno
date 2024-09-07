09867ea6bdf588660d66f1c071bb4a993457fee4
埃拉 <ElaBosak233@qq.com>
2024-09-07 09:28:43 +0800

    fix: datetimepicker's border

--- src/components/atoms/DatetimePicker.tsx
+++ src/components/atoms/DatetimePicker.tsx
@@ -156,102 +156,116 @@ export default function DatetimePicker(props: DatetimePickerProps) {
                     exitActive: styles["exit-active"],
                 }}
             >
-                <div className={styles["picker-container"]} ref={pickerRef}>
-                    <div className={styles["date-picker"]}>
-                        <div className={styles["month-year"]}>
-                            <button
-                                onClick={() =>
-                                    setSelectedDateTime(
-                                        selectedDateTime.minus({
-                                            months: 1,
-                                        })
-                                    )
-                                }
-                            >
-                                <ArrowLeftLinear />
-                            </button>
-                            <span>
-                                {selectedDateTime.toFormat("MMMM yyyy")}
-                            </span>
-                            <button
-                                onClick={() =>
-                                    setSelectedDateTime(
-                                        selectedDateTime.plus({ months: 1 })
-                                    )
-                                }
-                            >
-                                <ArrowRightLinear />
-                            </button>
-                        </div>
-                        <div className={styles["weekdays"]}>
-                            {[
-                                "Sun",
-                                "Mon",
-                                "Tue",
-                                "Wed",
-                                "Thu",
-                                "Fri",
-                                "Sat",
-                            ].map((day) => (
-                                <div key={day} className={styles["weekday"]}>
-                                    {day}
-                                </div>
-                            ))}
-                        </div>
-                        <div className={styles["days"]}>
-                            {Array.from(
-                                { length: firstDayOfMonth - 1 },
-                                (_, i) => (
-                                    <div
-                                        key={`empty-${i}`}
-                                        className={`${styles["day"]} ${styles["empty"]}`}
-                                    ></div>
-                                )
-                            )}
-                            {Array.from({ length: daysInMonth }, (_, i) => (
-                                <div
-                                    key={i + 1}
-                                    className={`${styles["day"]} ${selectedDateTime.day === i + 1 ? styles["selected"] : ""}`}
-                                    onClick={() => handleDateChange(i + 1)}
+                <div className={styles["container"]} ref={pickerRef}>
+                    <div className={styles["wrapper"]}>
+                        <div className={styles["date-picker"]}>
+                            <div className={styles["month-year"]}>
+                                <button
+                                    onClick={() =>
+                                        setSelectedDateTime(
+                                            selectedDateTime.minus({
+                                                months: 1,
+                                            })
+                                        )
+                                    }
                                 >
-                                    {i + 1}
-                                </div>
-                            ))}
+                                    <ArrowLeftLinear />
+                                </button>
+                                <span>
+                                    {selectedDateTime.toFormat("MMMM yyyy")}
+                                </span>
+                                <button
+                                    onClick={() =>
+                                        setSelectedDateTime(
+                                            selectedDateTime.plus({ months: 1 })
+                                        )
+                                    }
+                                >
+                                    <ArrowRightLinear />
+                                </button>
+                            </div>
+                            <div className={styles["weekdays"]}>
+                                {[
+                                    "Sun",
+                                    "Mon",
+                                    "Tue",
+                                    "Wed",
+                                    "Thu",
+                                    "Fri",
+                                    "Sat",
+                                ].map((day) => (
+                                    <div
+                                        key={day}
+                                        className={styles["weekday"]}
+                                    >
+                                        {day}
+                                    </div>
+                                ))}
+                            </div>
+                            <div className={styles["days"]}>
+                                {Array.from(
+                                    { length: firstDayOfMonth - 1 },
+                                    (_, i) => (
+                                        <div
+                                            key={`empty-${i}`}
+                                            className={`${styles["day"]} ${styles["empty"]}`}
+                                        ></div>
+                                    )
+                                )}
+                                {Array.from({ length: daysInMonth }, (_, i) => (
+                                    <div
+                                        key={i + 1}
+                                        className={`${styles["day"]} ${selectedDateTime.day === i + 1 ? styles["selected"] : ""}`}
+                                        onClick={() => handleDateChange(i + 1)}
+                                    >
+                                        {i + 1}
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                    <div className={styles["time-picker"]}>
-                        <input
-                            type="text"
-                            value={timeInputs.hour}
-                            onChange={(e) =>
-                                handleTimeInputChange("hour", e.target.value)
-                            }
-                            onBlur={applyChanges}
-                            onWheel={(e) => handleWheelChange(e, "hour")}
-                            maxLength={2}
-                        />
-                        <span className={styles["time-separator"]}>:</span>
-                        <input
-                            type="text"
-                            value={timeInputs.minute}
-                            onChange={(e) =>
-                                handleTimeInputChange("minute", e.target.value)
-                            }
-                            onBlur={applyChanges}
-                            onWheel={(e) => handleWheelChange(e, "minute")}
-                            maxLength={2}
-                        />
-                        <span className={styles["time-separator"]}>:</span>
-                        <input
-                            type="text"
-                            value={timeInputs.second}
-                            onChange={(e) =>
-                                handleTimeInputChange("second", e.target.value)
-                            }
-                            onBlur={applyChanges}
-                            onWheel={(e) => handleWheelChange(e, "second")}
-                            maxLength={2}
-                        />
+                        <div className={styles["time-picker"]}>
+                            <input
+                                type="text"
+                                value={timeInputs.hour}
+                                onChange={(e) =>
+                                    handleTimeInputChange(
+                                        "hour",
+                                        e.target.value
+                                    )
+                                }
+                                onBlur={applyChanges}
+                                onWheel={(e) => handleWheelChange(e, "hour")}
+                                maxLength={2}
+                            />
+                            <span className={styles["time-separator"]}>:</span>
+                            <input
+                                type="text"
+                                value={timeInputs.minute}
+                                onChange={(e) =>
+                                    handleTimeInputChange(
+                                        "minute",
+                                        e.target.value
+                                    )
+                                }
+                                onBlur={applyChanges}
+                                onWheel={(e) => handleWheelChange(e, "minute")}
+                                maxLength={2}
+                            />
+                            <span className={styles["time-separator"]}>:</span>
+                            <input
+                                type="text"
+                                value={timeInputs.second}
+                                onChange={(e) =>
+                                    handleTimeInputChange(
+                                        "second",
+                                        e.target.value
+                                    )
+                                }
+                                onBlur={applyChanges}
+                                onWheel={(e) => handleWheelChange(e, "second")}
+                                maxLength={2}
+                            />
+                        </div>
                     </div>
                 </div>
             </CSSTransition>
