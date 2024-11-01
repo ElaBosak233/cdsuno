263b87112e0346b7a3a5fb97d3f7b7652ac86d3c
埃拉 <ElaBosak233@qq.com>
2024-11-01 20:22:59 +0800

    wip: host 0.0.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
 
     return {
         server: {
+            host: "0.0.0.0",
             proxy: {
                 "/api": {
                     target: apiUrl,
