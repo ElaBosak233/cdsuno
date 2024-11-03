4905f5f47fbc39525051e94380d8c2248332c986
埃拉 <ElaBosak233@qq.com>
2024-11-03 18:53:12 +0800

    wip: poster

---
+++ src/components/core/Image/Image.tsx
@@ -0,0 +1,47 @@
+import React, { ComponentProps, useState } from "react";
+import styles from "./Image.module.scss";
+import Loading from "@/components/icons/Loading";
+
+export interface ImageProps extends ComponentProps<"img"> {
+    src?: string;
+    width?: string;
+    height?: string;
+    fallback?: React.ReactNode;
+}
+
+export function Image(props: ImageProps) {
+    const { src, width = "fit-content", height = "100%", style } = props;
+
+    const [err, setErr] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const variables = {
+        "--width": width,
+        "--height": height,
+    } as React.CSSProperties;
+
+    return (
+        <div
+            className={styles["root"]}
+            style={{
+                ...variables,
+                ...style,
+            }}
+        >
+            <img
+                src={src}
+                alt={""}
+                onLoad={() => setLoading(false)}
+                onError={() => setErr(true)}
+                draggable={false}
+            />
+            {loading && (
+                <div className={styles["loading"]}>
+                    <div className={styles["loading-icon"]}>
+                        <Loading />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
--- src/pages/_games/page.tsx
+++ src/pages/_games/page.tsx
@@ -1,3 +1,37 @@
+import { Image } from "@/components/core/Image";
+import styles from "./page.module.scss";
+import { Button } from "@/components/core";
+import PlayCircleBold from "~icons/solar/play-circle-bold";
+
 export function Page() {
-    return <div>games</div>;
+    return (
+        <>
+            <h1>近期赛事</h1>
+            <div className={styles["main"]}>
+                <div className={styles["poster"]}>
+                    <Image
+                        src="https://picsum.photos/1920/1080"
+                        width="50%"
+                        height="60vh"
+                        style={{
+                            boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+                        }}
+                    />
+                    <div className={styles["info"]}>
+                        <span>CdsCTF 2024</span>
+                    </div>
+                    <div className={styles["enter"]}>
+                        <Button
+                            icon={<PlayCircleBold />}
+                            variant={"outlined"}
+                            width="100%"
+                            height="100%"
+                        >
+                            进入
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
